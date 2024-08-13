56caefcd615bd27d9b9921c2fedfe2ba0b46be31
최호 <67588757+choihooo@users.noreply.github.com>
2024-08-13 17:36:10 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.48 ms, Memory: 80.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+
+        int midIndex = array.length / 2;
+
+        return array[midIndex];
+    }
+}
