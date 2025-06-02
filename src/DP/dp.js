6d9ab0a953237756047956672ad742bf603a0c23
choihooo <hochoi8621@naver.com>
2025-06-02 13:26:34 +0900

    dp 구현:

--- src/DP/dp.js
+++ src/DP/dp.js
@@ -1,4 +1,6 @@
 d = new Array(100).fill(0)
+
+// 하향식
 function fibo(x) {
     if (x == 1 || x == 2) {
         return 1;
@@ -11,3 +13,19 @@ function fibo(x) {
     d[x] = fibo(x - 1) + fibo(x - 2)
     return d[x]
 }
+
+// 상향식
+d[1] = 1
+d[2] = 1
+n = 99
+
+for (let i = 3; i <= n; i++) {
+    d[i] = d[i - 1] + d[i - 2]
+}
+
+
+/*
+1. 종료 조건
+2. 이미 계산한식인지 판별
+3. 계산
+*/
